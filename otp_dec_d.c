14f6f3996661fe15e46cf49c23efebbb45e7e860
wwang126 <wavelet.wang@gmail.com>
2017-12-01 23:34:59 -0800

    updated dec

--- otp_dec_d.c
+++ otp_dec_d.c
@@ -6,13 +6,56 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+char* encode(char* plainText, char* keyText){
+	if(strlen(keyText) < strlen(plainText)){
+		perror("Key too small!");
+		exit(1);
+	}
+	char* encodeText = malloc(strlen(plainText)+1);
+	int i = 0;
+	char in = ' ';
+	while(plainText[i] != '\0'){
+		in = plainText[i];
+		char key = keyText[i];
+		//Convert chars into numbers
+		if(in == ' '){
+			in = 26;
+		}
+		else{
+			in = in - 65;
+		}
+		//convert keys into numbers
+		if(key == ' '){
+			key = 26;
+		}
+		else{
+			key = key - 65;
+		}
+		//Encode the character
+		char encode = (in + key)%27;
+		//Convert character back to char
+		if(encode == 26){
+			encode = ' ';
+		}
+		else{
+			encode = encode - 65;
+		}
+		encodeText[i] = encode;
+		i++;
+	}
+	return encodeText;
+}
+
 void error(const char *msg) { perror(msg); exit(1); } // Error function used for reporting issues
 
 int main(int argc, char *argv[])
 {
 	int listenSocketFD, establishedConnectionFD, portNumber, charsRead;
 	socklen_t sizeOfClientInfo;
-	char buffer[256];
+	//Create storage from strings
+	char buffer[200000];
+	char plainText[200000];
+	char keyText[200000];
 	struct sockaddr_in serverAddress, clientAddress;
 
 	if (argc < 2) { fprintf(stderr,"USAGE: %s port\n", argv[0]); exit(1); } // Check usage & args
@@ -35,19 +78,85 @@ int main(int argc, char *argv[])
 
 	// Accept a connection, blocking if one is not available until one connects
 	sizeOfClientInfo = sizeof(clientAddress); // Get the size of the address for the client that will connect
-	establishedConnectionFD = accept(listenSocketFD, (struct sockaddr *)&clientAddress, &sizeOfClientInfo); // Accept
-	if (establishedConnectionFD < 0) error("ERROR on accept");
 
-	// Get the message from the client and display it
-	memset(buffer, '\0', 256);
-	charsRead = recv(establishedConnectionFD, buffer, 255, 0); // Read the client's message from the socket
-	if (charsRead < 0) error("ERROR reading from socket");
-	printf("SERVER: I received this from the client: \"%s\"\n", buffer);
+	/**Start Accepting connections here**/
 
-	// Send a Success message back to the client
-	charsRead = send(establishedConnectionFD, "I am the server, and I got your message", 39, 0); // Send success back
-	if (charsRead < 0) error("ERROR writing to socket");
-	close(establishedConnectionFD); // Close the existing socket which is connected to the client
-	close(listenSocketFD); // Close the listening socket
+	while(1){
+		//kill a hanging process, since you only accept one proccess at a time
+		int status;
+		pid_t pidWait = waitpid(-1, &status, WNOHANG);
+
+		establishedConnectionFD = accept(listenSocketFD, (struct sockaddr *)&clientAddress, &sizeOfClientInfo); // Accept
+		if (establishedConnectionFD < 0) error("ERROR on accept");
+
+		//Fork after accepting connection
+		pid_t pid = fork();
+		//If child
+		if(pid == 0){
+			// Get the message from the client and display it
+			memset(buffer, '\0', 200000);
+			do{
+				//Read in a packet of size 64
+				charsRead = recv(establishedConnectionFD, buffer + strlen(buffer), 64, 0);
+				if (charsRead < 0) error("ERROR reading from socket");
+				if (strstr(buffer, "^") > 0){
+					break;
+				}
+			}while(charsRead > 0);
+			//If wrong
+			if(buffer[0] != '&'){
+				// Send message back
+				memset(buffer, '\0',200000);
+				charsRead = send(establishedConnectionFD, "Wrong Server!^", 14, 0);
+				if (charsRead < 0) error("ERROR writing to socket");
+				close(establishedConnectionFD); // Close the existing socket which is connected to the client
+				close(listenSocketFD); // Close the listening socket
+				exit(0);//Kill child
+			}
+			//Get plain text
+			memset(plainText, '\0', 200000);
+			char curr = '0';
+			int i = 0;
+			while(curr != '%'){
+				curr = buffer[i+1];
+				plainText[i] = curr;
+				i++;
+			}
+			plainText[i - 1] = '\0';
+
+			//Get key text
+			memset(keyText, '\0',200000);
+			int j = 0;
+			while(curr != '^'){
+				curr = buffer[i+1];
+				keyText[j] = curr;
+				i++;
+				j++;
+			}
+			keyText[j - 1] = '\0';
+			//Encrypt the text
+			char* encodeText = encode(plainText,keyText);
+			//Add end text
+			encodeText[strlen(encodeText)] = '^';
+
+			// Send message back
+			memset(buffer, '\0',200000);
+			strcpy(buffer,encodeText);
+			charsRead = send(establishedConnectionFD, buffer, strlen(buffer), 0); // Send success back
+			if (charsRead < 0) error("ERROR writing to socket");
+			close(establishedConnectionFD); // Close the existing socket which is connected to the client
+			close(listenSocketFD); // Close the listening socket
+			exit(0);//Kill child
+		}
+		//If parent
+		else if(pid > 0){
+			//do nothing
+		}
+		//If error
+		else{
+			error("SERVER: Fork Error!");
+		}
+	}
+	//TODO: Remove immortality
 	return 0;
 }
