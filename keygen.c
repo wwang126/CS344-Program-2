a1db2d5406732e77cf152dc237d544b6abdb5050
wwang126 <wavelet.wang@gmail.com>
2017-12-01 17:20:06 -0800

    add keygen file

---
+++ keygen.c
@@ -0,0 +1,27 @@
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (int argc, char* argv[]){
+    //convert arg into int val
+    int keyLen = atoi(argv[1]);
+
+    printf("Number is :%d\n", keyLen);
+    int i = 0;
+    //Seed rng
+    srand(time(NULL));
+    while(i < keyLen){
+        int randInt = rand();
+        randInt = randInt % 27;
+        if(randInt == 26){
+            printf(" ");
+        }
+        else{
+            //print out character
+            printf("%c", randInt + 65);
+        }
+        i++;
+    }
+    //print out newline
+    printf("\n");
+}
